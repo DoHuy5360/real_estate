df28af35d726ff8a3c5980eb96bbe5a4e4776cc9
Dohuy5360 <loldh321@gmail.com>
2022-07-16 15:05:00 +0700

    rename 'name' to 'project_name'

--- database/migrations/2022_07_10_111556_create_projects_table.php
+++ database/migrations/2022_07_10_111556_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('project_name');
             $table->string('price');
             $table->string('source');
             $table->string('creator');
